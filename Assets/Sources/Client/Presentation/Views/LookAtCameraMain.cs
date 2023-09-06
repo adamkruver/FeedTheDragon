a9d0f1bac069cba9a5560fa2c7b9a362f2d120ac
Алексей Кононов <kruver@mail.ru>
2023-09-06 15:37:36 +0300

    Added Inventory & Some UI

---
+++ Assets/Sources/Client/Presentation/Views/LookAtCameraMain.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Sources.Client.Presentation.Views
+{
+    public class LookAtCameraMain : MonoBehaviour
+    {
+        private Transform _transform;
+        private Transform _cameraTransform;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _transform = GetComponent<Transform>();
+            _camera = Camera.main;
+            _cameraTransform = _camera.GetComponent<Transform>();
+        }
+
+        private void Update()
+        {
+            _transform.rotation = _cameraTransform.rotation;
+        }
+    }
+}
