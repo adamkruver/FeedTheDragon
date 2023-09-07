04274c9ded22595c3eb02b9915df855874e8997d
Алексей Кононов <kruver@mail.ru>
2023-09-07 11:02:21 +0300

    Added Mushroom Animations

---
+++ Assets/Sources/Client/Presentation/Views/LineRendererPoints.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Sources.Client.Presentation.Views
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class LineRendererPoints : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _points;
+        private LineRenderer _lineRenderer;
+
+        private void Awake()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+
+            _lineRenderer.positionCount = _points.Length;
+        }
+
+        private void FixedUpdate()
+        {
+            for (int i = 0; i < _points.Length; i++)
+            {
+                Vector3 position = _points[i].position;
+                _lineRenderer.SetPosition(i, position);
+            }
+        }
+    }
+}
