2a2b97e64488c094411c2d88ed0af98a01b19e05
Алексей Кононов <kruver@mail.ru>
2023-09-13 21:09:50 +0300

    Added DiffExtension

---
+++ Assets/Sources/Client/Extensions/Diff.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sources.Client.Extensions
+{
+    public static partial class Extensions
+    {
+        public static (IEnumerable<T> Added, IEnumerable<T> Removed) Diff<T>(this IEnumerable<T> sourceCollection,
+            IEnumerable<T> changedCollection, Func<T, T, bool> comparer)
+        {
+            List<T> removed = new List<T>();
+            List<T> targets = changedCollection.ToList();
+
+            foreach (T item in sourceCollection)
+            {
+                T sameItem = targets.FirstOrDefault(targetItem => comparer.Invoke(targetItem, item));
+
+                if (sameItem == null)
+                    removed.Add(item);
+                else
+                    targets.Remove(sameItem);
+            }
+
+            return (targets, removed);
+        }
+    }
+}
